15d34799cbd13914a6955cd96e370b8ab3c7e61d
jordyakadiak <83762281+jordyakadiak@users.noreply.github.com>
2023-04-01 01:45:24 +0200

    Update server.js
     L'idée est de diviser le code en plusieurs parties pour faciliter la maintenance et l'évolutivité du controller. le fichier route.js  appelle les fonctions du fichier services.js en fonction des requêtes HTTP et les fichier server.js possède  les fonctions qui interagissent avec la base de données du coup cette approche a l'avantage de simplifier le code et de permettre une meilleure organisation de notre API.

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-// Charger les modules http et mysql
-let http = require("http");
 let mysql = require("mysql");
 
-// Connectez-vous à la base de données
+// Connect to database
 let connection = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -10,196 +8,84 @@ let connection = mysql.createConnection({
   database: "todolist",
 });
 
-// Connecter à la base de données
-dbConn.connect(function (err) {
-  if (err) throw err;
-  console.log("Connecté à la base de données MySQL!");
-});
-
-// Créer un serveur
-const server = http.createServer(function (req, res) {
-  // GET /tasks : Récupérer toutes les tâches
-  if (req.method === "GET" && req.url === "/tasks") {
-    getAllTasks(req, res);
-  }
-
-  // GET /tasks/:id : Récupérer une tâche spécifique
-  else if (req.method === "GET" && /^\/tasks\/\d+$/.test(req.url)) {
-    const taskId = parseInt(req.url.split("/")[2]);
-    getTask(req, res, taskId);
-  }
-
-  // POST /tasks : Créer une nouvelle tâche
-  else if (req.method === "POST" && req.url === "/tasks") {
-    createTask(req, res);
-  }
-
-  // PUT /tasks/:id : Mettre à jour une tâche existante
-  else if (req.method === "PUT" && /^\/tasks\/\d+$/.test(req.url)) {
-    const taskId = parseInt(req.url.split("/")[2]);
-    updateTask(req, res, taskId);
-  }
-
-  // DELETE /tasks/:id : Supprimer une tâche spécifique
-  else if (req.method === "DELETE" && /^\/tasks\/\d+$/.test(req.url)) {
-    const taskId = parseInt(req.url.split("/")[2]);
-    deleteTask(req, res, taskId);
-  }
-
-  // Sinon, renvoyer une erreur 404
-  else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 Not Found\n");
-  }
-});
-
-// Écouter le port 5500
-server.listen(5500);
-
-console.log("Le serveur est démarré sur le port 5500...");
-
-// Récupérer toutes les tâches
-function getAllTasks(req, res) {
-  dbConn.query("SELECT * FROM todolist", function (err, results, fields) {
-    if (err) throw err;
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(results));
-  });
-}
-// Récupérer toutes les tâches
-async function getAllTasks(req, res) {
+// Get all tasks
+async function getAllTasks() {
   try {
-    const [rows] = await dbConn.promise().query("SELECT * FROM todolist");
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(rows));
+    const [rows] = await connection.promise().query("SELECT * FROM todolist");
+    return rows;
   } catch (err) {
     console.error(err.message);
-    res.writeHead(500, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        error:
-          "Une erreur serveur s'est produite lors de la récupération de toutes les tâches.",
-      })
-    );
+    throw err;
   }
 }
 
-// Récupérer une tâche spécifique
-async function getTask(req, res, taskId) {
+// Get a specific task
+async function getTask(taskId) {
   try {
-    const [rows] = await dbConn
+    const [rows] = await connection
       .promise()
       .execute("SELECT * FROM todolist WHERE id = ?", [taskId]);
     if (rows.length === 0) {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: "La tâche demandée n'existe pas." }));
-    } else {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(rows));
+      throw new Error("La tâche demandée n'existe pas.");
     }
+    return rows[0];
   } catch (err) {
     console.error(err.message);
-    res.writeHead(500, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        error:
-          "Une erreur serveur s'est produite lors de la récupération de la tâche.",
-      })
-    );
+    throw err;
   }
 }
 
-// Créer une nouvelle tâche
-async function createTask(req, res) {
+// Create a task
+async function createTask(task) {
   try {
-    let body = "";
-    req.on("data", function (chunk) {
-      body += chunk.toString();
-    });
-    req.on("end", async function () {
-      const task = JSON.parse(body);
-      const [result] = await dbConn
-        .promise()
-        .execute("INSERT INTO todolist (titre) VALUES (?)", [task.titre]);
-      res.writeHead(201, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ id: result.insertId, ...task }));
-    });
+    const [result] = await connection
+      .promise()
+      .execute("INSERT INTO todolist (titre) VALUES (?)", [task.titre]);
+    return { id: result.insertId, ...task };
   } catch (err) {
     console.error(err.message);
-    res.writeHead(500, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        error:
-          "Une erreur serveur s'est produite lors de la création de la tâche.",
-      })
-    );
+    throw err;
   }
 }
 
-// Mettre à jour une tâche existante
-async function updateTask(req, res, taskId) {
+// Update a task
+async function updateTask(taskId, task) {
   try {
-    let body = "";
-    req.on("data", function (chunk) {
-      body += chunk.toString();
-    });
-    req.on("end", async function () {
-      const task = JSON.parse(body);
-      const [result] = await dbConn
-        .promise()
-        .execute("UPDATE todolist SET titre = ? WHERE id = ?", [
-          task.titre,
-          taskId,
-        ]);
-      if (result.affectedRows === 0) {
-        res.writeHead(404, { "Content-Type": "application/json" });
-        res.end(
-          JSON.stringify({
-            error: "La tâche à mettre à jour n'a pas été trouvée.",
-          })
-        );
-      } else {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ id: taskId, ...task }));
-      }
-    });
+    const [result] = await connection
+      .promise()
+      .execute("UPDATE todolist SET titre = ? WHERE id = ?", [
+        task.titre,
+        taskId,
+      ]);
+    if (result.affectedRows === 0) {
+      throw new Error("La tâche à mettre à jour n'a pas été trouvée.");
+    }
+    return { id: taskId, ...task };
   } catch (err) {
     console.error(err.message);
-    res.writeHead(500, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        error:
-          "Une erreur serveur s'est produite lors de la mise à jour de la tâche.",
-      })
-    );
+    throw err;
   }
 }
 
-// Supprimer une tâche spécifique
-async function deleteTask(req, res, taskId) {
+// Delete a task
+async function deleteTask(taskId) {
   try {
-    const [result] = await dbConn
+    const [result] = await connection
       .promise()
       .query("DELETE FROM todolist WHERE id = ?", [taskId]);
     if (result.affectedRows === 0) {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          error: "La tâche demandée à supprimer n'a pas été trouvée.",
-        })
-      );
-    } else {
-      res.writeHead(204);
-      res.end();
+      throw new Error("La tâche demandée à supprimer n'a pas été trouvée.");
     }
   } catch (err) {
     console.error(err.message);
-    res.writeHead(500, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        error:
-          "Une erreur serveur s'est produite lors de la suppression de la tâche.",
-      })
-    );
+    throw err;
   }
 }
+
+module.exports = {
+  getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+};
